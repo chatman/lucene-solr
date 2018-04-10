3a1d6cf6c8d23dc322d23860016737f26f9ce696
Ishan Chattopadhyaya <ishan@apache.org>
2018-04-10 17:05:09 +0530

    SOLR-10317: Adding querying benchmarks capabilities

--- dev-tools/solrnightlybenchmarks/src/main/java/org/apache/solr/tests/nightlybenchmarks/BenchmarksMain.java
+++ dev-tools/solrnightlybenchmarks/src/main/java/org/apache/solr/tests/nightlybenchmarks/BenchmarksMain.java
@@ -1,11 +1,14 @@
 package org.apache.solr.tests.nightlybenchmarks;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.CloudSolrClient;
 import org.apache.solr.client.solrj.impl.ConcurrentUpdateSolrClient;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.common.params.ModifiableSolrParams;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -17,9 +20,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class BenchmarksMain {
 
@@ -27,7 +32,8 @@ public class BenchmarksMain {
 
     public static void main(String[] args) throws Exception {
 
-        String commitId = "e782082e711286a4c1a6ca101a9fa11bafab7b0d";
+        //String commitId = "e782082e711286a4c1a6ca101a9fa11bafab7b0d";
+    	String commitId = "a70b257c805b047588e133c5280ecfae418668b3";
 
         if (args.length>0 && args[0].length()==40) {
             commitId = args[0];
@@ -37,6 +43,7 @@ public class BenchmarksMain {
 
         try {
             solrCloud.init();
+            System.out.println("SolrCloud inited...");
 
             String commitDate = Util.getDateString(solrCloud.nodes.get(0).commitTime);
 
@@ -46,7 +53,6 @@ public class BenchmarksMain {
 
 
             JSONArray indexBenchmarks = (JSONArray)json.get("index-benchmarks");
-
             Iterator it = indexBenchmarks.iterator();
             while (it.hasNext()) {
                 JSONObject benchmark = (JSONObject)it.next();
@@ -78,7 +84,6 @@ public class BenchmarksMain {
                             solrCloud.createCollection(collectionName, null, shards, replicationFactor);
                             long start = System.nanoTime();
                             index (solrCloud, collectionName, i, datasetFile);
-                            queryBench(solrCloud,collectionName,i,json);
                             long end = System.nanoTime();
 
                             if (i != maxThreads) {
@@ -86,61 +91,69 @@ public class BenchmarksMain {
                             }
 
                             timings.put("Threads: " + i, String.valueOf((end-start)/10000000.0));
-                        } else {
-                            /*createCore(standalone, coreName);
- *                         index (standalone, collectionName, i, datasetFile, outputCSV);
- *                                                 if (i != maxThreads) {
- *                                                                             deleteCore(standalone, collectionName);
- *                                                                                                     }*/
-
-                        }
-
+                        } // non-cloud not supported as of now
                     }
 
                     System.out.println("Final metrics: "+timings);
                     Util.outputMetrics(outputCSV, timings);
                 }
-
             }
+            
+            System.out.println("Querying benchmarks...\n***********************\n****************");
 
+            // Query benchmarks
+            JSONArray queryBenchmarks = (JSONArray)json.get("query-benchmarks");
+            it = queryBenchmarks.iterator();
+            while (it.hasNext()) {
+                JSONObject benchmark = (JSONObject)it.next();
+
+                String name = (String)benchmark.get("name");
+                String description = (String)benchmark.get("description");
+                String queryFile = (String)benchmark.get("query-file");
+                String replicationType = (String)benchmark.get("replication-type");
+                String collection = (String)benchmark.get("collection");
+                int minThreads = (int)(long) benchmark.get("min-threads");
+                int maxThreads = (int)(long) benchmark.get("max-threads");
+
+                String outputCSV = "./src/main/webapp/data/" + name +".csv";
+
+                Map<String, String> timings = new LinkedHashMap<>();
+                timings.put("Date",  commitDate);
+                timings.put("Test_ID", commitId);
+                timings.put("CommitID",  commitId);
+
+                for (int threads = minThreads; threads <= maxThreads; threads++) {
+                	ExecutorService executor = Executors.newFixedThreadPool(threads);
+                	List<String> queries = FileUtils.readLines(new File(queryFile), "UTF-8");
+                	
+                	String solr1 = solrCloud.nodes.get(0).getBaseUrl()+collection;
+                	HttpSolrClient client = new HttpSolrClient(solr1);
+
+                	long start = System.currentTimeMillis();
+                	for (String query : queries) {
+                		Runnable worker = new QueryThread(client, query, collection);
+                		executor.execute(worker);
+                	}
+                	executor.shutdown();
+                	executor.awaitTermination(10, TimeUnit.SECONDS);
+                	client.close();
+                	long time = System.currentTimeMillis() - start;
+                	System.out.println("Took time: " + time);
+                	if (time > 0) {
+                		double qps = queries.size() / (time / 1000.0);
+                		System.out.println("QPS: " + qps+", Thread: "+threads);
+                        timings.put("Threads: " + threads, String.valueOf(qps));
+                	}
+                }
+                Util.outputMetrics(outputCSV, timings);
+            }
+            
         } finally {
             solrCloud.shutdown();
         }
 
         FileUtils.copyFile(new File("config.json"), new File("src/main/webapp/data/config.json"));
     }
-
-    private static void queryBench(SolrCloud solrCloud, String collectionName, int i, JSONObject json) throws IOException {
-        JSONArray queryBenchmarks = (JSONArray) json.get("query-benchmarks");
-        Iterator it = queryBenchmarks.iterator();
-        while (it.hasNext()) {
-            JSONObject benchmark = (JSONObject) it.next();
-            int minThreads = (int) (long) benchmark.get("min-threads");
-            int maxThreads = (int) (long) benchmark.get("max-threads");
-            String datasetFile = (String) benchmark.get("query-file");
-            for (i = minThreads; i <= maxThreads; i++) {
-                query(solrCloud, collectionName, datasetFile, i);
-            }
-        }
-    }
-
-    private static void query(SolrCloud solrCloud, String collectionName, String datasetFile, int i) throws IOException {
-        ExecutorService executor = Executors.newFixedThreadPool(i);
-        File file = new File(datasetFile);
-        ArrayList<String> queries = fileToList(file);
-        Long st = System.currentTimeMillis();
-        for (String query : queries) {
-            Runnable worker = new QueryThread( query,collectionName);
-            executor.execute(worker);
-        }
-        executor.shutdown();
-        while (!executor.isTerminated()) {
-        }
-Long time=((System.currentTimeMillis() - st) / 1000);
-        if(time>0) {
-            System.out.println("QPS:" + new Float(queries.size() / time)+" Thread:"+i);
-}    
-}
 
     private static ArrayList<String> fileToList(File file) throws IOException {
         ArrayList<String> queries = new ArrayList<>();
@@ -187,21 +200,19 @@ Long time=((System.currentTimeMillis() - st) / 1000);
 class QueryThread implements Runnable {
     private String query;
     private String collectionName;
-    public QueryThread( String query,String collectionName) {
+    private SolrClient client;
+    
+    public QueryThread( SolrClient client, String query,String collectionName) {
         this.query = query;
         this.collectionName=collectionName;
+        this.client = client;
     }
 
     public void run() {
         SolrQuery q = new SolrQuery(query);
         try {
-            CloudSolrClient cloudClient = new CloudSolrClient.Builder()
-                    .withZkHost("127.0.0.1:2181").build();
-            cloudClient.connect();
-            cloudClient.setDefaultCollection(collectionName);
-            Long count = cloudClient.query(q).getResults().getNumFound();
-         //   System.out.println("Query:" + query + " count:" + count);
-            cloudClient.close();
+        	long count = client.query(q).getResults().getNumFound();
+        	System.out.println("Query:" + query + " count:" + count);
         } catch (SolrServerException e) {
             e.printStackTrace();
         } catch (IOException e) {
